923cd3d8ef4d726d4f4738c668c90b0500271328
Callie Porter-Borden <porterhyphenborden@gmail.com>
2019-03-08 23:13:29 -0500

    Initial commit of quiz app

---
+++ datastore.js
@@ -0,0 +1,115 @@
+const STORE = [
+    {
+      question: 'What type of plant is used to make tequila?',
+      answers: [
+        'Aloe',
+        'Sugar cane',
+        'Agave',
+        'Corn'
+      ],
+      correctAnswer: 'answer-three'
+    },
+  
+    {
+      question: 'Where does tequila get its name?',
+      answers: [
+        'The town called Tequila, where much of the liquor is produced.',
+        'Eduardo Tequila, the first person to mass produce the spirit.',
+        'The aztec deity Tequila who was associated with the agave plant.',
+        'The San Antonio tavern Tequila that first popularized the beverage.'
+      ],
+      correctAnswer: 'answer-one'
+    },
+  
+    {
+      question: 'On average, how many years does it take for an agave plant to reach maturity for harvest?',
+      answers: [
+        '2-3',
+        '4-6',
+        '8-10',
+        '15-20'
+      ],
+      correctAnswer: 'answer-three'
+    },
+  
+    {
+      question: `Which of the following matches these categories of tequila:<br>Aged 1-2 months / Aged 2 - 12 months / Aged 1 - 3 years`,
+      answers: [
+        'Joven / mediano / viejo',
+        'Silver / bronze / gold',
+        'Reposado / anejo / muy anejo',
+        'Blanco / reposado / anejo'
+      ],
+      correctAnswer: 'answer-four'
+    },
+  
+    {
+      question: `Put these events in the correct order of tequila production:<br>
+      A: The agave is shredded and the juice is extracted.<br>
+      B: The liquid, called mosto, is distilled.<br>
+      C: The agave is cooked to convert complex sugars to simple sugars.<br>
+      D: The juice is fermented, converting sugar into alcohol.`,
+      answers: [
+        'A - C - D - B',
+        'C - A - D - B',
+        'C - A - B - D',
+        'A - D - C - B'
+      ],
+      correctAnswer: 'answer-two'
+    },
+  
+    {
+      question: 'Which of the following is NOT a key difference between tequila and mezcal?',
+      answers: [
+        'Tequila is made from the whole, roasted agaves, while mezcal uses the sugary sap from the plant.',
+        'Tequila must be made from Blue Agave while mezcal can be made from many different types of agave.',
+        'Tequila primarily comes from the Mexican state of Jalisco, while mezcal production is centered in Oaxaca.',
+        'For tequila, the agave is baked in ovens. For mezcal it is roasted in earthen pits, often lending a smokey flavor.'
+      ],
+      correctAnswer: 'answer-one'
+    },
+  
+    {
+      question: 'Which of the following is NOT a spirit made from agave?',
+      answers: [
+        'Cachaca',
+        'Raicilla',
+        'Mezcal',
+        'Bacanora'
+      ],
+      correctAnswer: 'answer-one'
+    },
+  
+    {
+      question: 'Which of these statements is true?',
+      answers: [
+        'Mezcal is a specific type of Tequila.',
+        'Mezcal and Tequila are the same thing.',
+        'Tequila is a specific type of Mezcal.',
+        'Mezcal and Tequila are completely separate spirits.'
+      ],
+      correctAnswer: 'answer-three'
+    },
+  
+    {
+      question: 'What is the fermented beverage made from agave sap (aguamiel) called?',
+      answers: [
+        'Sotol',
+        'Ordinario',
+        'Mosto',
+        'Pulque'
+      ],
+      correctAnswer: 'answer-four'
+    },
+  
+    {
+      question: 'Which of the following is NOT true of the quiote, the tall flowering stalk that grows from the agave plant?',
+      answers: [
+        'It is commonly cut down to prevent sugars being diverted away from the agave pina.',
+        'It is how the agave plant sexually reproduces and its flowers are fertilized by bats and hummingbirds.',
+        'It can be cooked and eaten, or dried and used similarly to wooden posts.',
+        'It is cut down and used to make a special type of mezcal.'
+      ],
+      correctAnswer: 'answer-four'
+    }
+  ];
---
+++ script.js
@@ -0,0 +1,156 @@
+let questionNum = 0;
+let score = 0;
+
+// Loads question and answer content to page
+function loadQuestion() {
+  console.log('`loadQuestion` ran');
+  if (questionNum < STORE.length) {
+    $('.nextButton').hide();
+    $('.feedback').hide();
+    $('.questionAnswerForm').html(`
+        <form>
+            <h2>${STORE[questionNum].question}</h2>
+            <fieldset name="answer options">
+                <label for="answer-one">
+                    <input type="radio" name="answer" id="answer-one" value="${STORE[questionNum].answers[0]}" required>
+                    <span>${STORE[questionNum].answers[0]}</span>
+                </label>
+                <label for="answer-two">
+                    <input type="radio" name="answer" id="answer-two" value="${STORE[questionNum].answers[1]}" required>
+                    <span>${STORE[questionNum].answers[1]}</span>
+                </label>
+                <label for="answer-three">
+                    <input type="radio" name="answer" id="answer-three" value="${STORE[questionNum].answers[2]}" required>
+                    <span>${STORE[questionNum].answers[2]}</span>
+                </label>
+                <label for="answer-four">
+                    <input type="radio" name="answer" id="answer-four" value="${STORE[questionNum].answers[3]}" required>
+                    <span>${STORE[questionNum].answers[3]}</span>
+                </label>
+                <button type="submit" class="submitButton">SUBMIT</button>
+            </fieldset>
+        </form>`);
+    $('.questionTracker').html(`Question ${questionNum+1}/10`);
+    $('.scoreTracker').html(`Score: ${score}`);
+  }
+}
+
+// Removes initial elements and runs function to load question and answer form
+function handleStart() {
+  console.log('`handleStart` ran');
+  $('.startButton').on('click', function(event) {
+    $('.js-initialDisplay').remove();
+    $('.questionAnswerForm').css('display', 'block');
+    $('.progressTracker').css('display', 'inherit');
+    $('body').css({"background-image": "url('https://i.imgur.com/PGgE8GX.png')", "background-size": "450px", "background-position": "center bottom"});
+    loadQuestion();
+  })
+}
+
+// Increases score by one point
+function incrementScore() {
+  score ++;
+}
+
+// Displays feedback for correct answer
+function feedbackCorrect() {
+  $('.submitButton').hide();
+  $('fieldset').before(`<div class="feedback correct">CORRECT!</div>`);
+  incrementScore(score);
+  $('.scoreTracker').text(`Score: ${score}`);
+  $('input:checked').parent().addClass('correctAns');
+  $('fieldset').append(`<button class="nextButton">NEXT</button>`);
+}
+
+// Displays feedback for incorrect answer
+function feedbackIncorrect() {
+  $('.submitButton').hide();
+  $('fieldset').before(`<div class="feedback incorrect">INCORRECT!</div>`);
+  $('input:checked').parent().addClass('incorrectAns');
+  let correctID = STORE[questionNum].correctAnswer;
+  //console.log(correctID);
+  $('#' + correctID).parent().addClass('correctAns');
+  $('fieldset').append(`<button class="nextButton">NEXT</button>`);
+}
+
+// Verifies answer and determines feedback
+function handleSubmit() {
+  console.log('`handleSubmit` ran');
+  $('.questionAnswerForm').on('submit', function(event) {
+      event.preventDefault();
+      let selected = $('input:checked').attr('id');
+      //console.log(`${selected}`);
+      let correctAns = `${STORE[questionNum].correctAnswer}`;
+      //console.log(`${correctAns}`);
+      if (selected === correctAns) {
+        feedbackCorrect();
+      }
+      else {
+        feedbackIncorrect();
+      }
+  });
+}
+
+// Increases question number by one
+function incrementQuestion() {
+  questionNum ++;
+}
+
+// Displays results at end of quiz
+function showResults() {
+    $('body').css("background-image", "none");
+    $('.progressTracker').hide();
+    if (score == 10) {
+        $('.questionAnswerForm').html(`
+            <h2>You got a perfect score!</h2>
+            <p>You are a true afficianado. Go have some tequila to celebrate!
+            <img src="https://i.imgur.com/NjTvUiO.png" alt="Bottle of Tequila"></p>
+            <button class="restartButton">RETAKE QUIZ</button>`);
+  }
+    else if (score < 10 && score >= 6) {
+        $('.questionAnswerForm').html(`
+            <h2>You got ${score}/10 questions correct!</h2>
+            <p>You're on your way to becoming an expert. Have a margarita while you study!
+            <img src="https://i.imgur.com/D4knvng.png" alt="Margarita"></p>
+            <button class="restartButton">RETAKE QUIZ</button>`);
+  }
+  else {
+        $('.questionAnswerForm').html(`
+            <h2>You got ${score}/10 questions correct!</h2>
+            <p>No more tequila for you! Stick to water from now on.
+            <img src="https://i.imgur.com/IPqunfn.png" alt="Glass of water"></p>
+            <button class="restartButton">RETAKE QUIZ</button>`);
+  }
+}
+
+// Moves on to next question or results depending on questionNum
+function handleNext() {
+  $('.questionAnswerForm').on('click', '.nextButton', function(event) {
+    incrementQuestion();
+    if (questionNum < STORE.length) {
+      loadQuestion();
+      //console.log(STORE.length);
+      //console.log(questionNum);
+    }
+    else {
+      showResults();
+    }
+  });
+}
+
+// Reloads quiz from beginning
+function retakeQuiz() {
+  $('.questionAnswerForm').on('click', '.restartButton', function (event) {
+    location.reload();
+  });
+}
+
+// Callback function for document ready
+function loadQuiz() {
+  handleStart();
+  handleSubmit();
+  handleNext();
+  retakeQuiz();
+}
+
+$(loadQuiz);
